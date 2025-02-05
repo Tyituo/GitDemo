3cd46b41c1ba1550db87176cb429ce7f3e498598
SITC-QBcourse <richyyang1314@shirahagi.ac.jp>
2025-02-05 11:20:35 +0900

    给demo01添加新的语句

--- src/com/koneko/consulting/demo/Demo01.java
+++ src/com/koneko/consulting/demo/Demo01.java
@@ -3,5 +3,6 @@ package com.koneko.consulting.demo;
 public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello QB20241a");
 	}
 }
